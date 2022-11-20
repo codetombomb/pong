c37a1357d2bf0b89bf83d969c3e3cccb85eb3d37
Tom Tobar <cocdetombomb@gmail.com>
2022-11-20 17:35:49 -0600

    add ball bounce trajectory

--- src/pong.js
+++ src/pong.js
@@ -26,22 +26,25 @@ export class Pong extends Phaser.Scene {
     
     this.ball.body.bounce.set(.9);
     this.leftPaddle.setScale(this.paddleSize);
+    
     this.ball.setScale(this.ballSize)
-
     this.ball.setCollideWorldBounds(true);
+    this.ball.setRandomPosition(267, 0, 267, 600)
+    this.ball.setVelocityX(Math.floor(Math.random() * 101) - 400)
+    this.ball.setVelocityY(Math.floor(Math.random() * 101) - 400)
   }
   update() {
     if (this.gameOver) return;
-    if (this.cursors.down.isDown && this.cursors.space.isDown) {
+    if (this.cursors.down.isDown && this.cursors.space.isDown && this.leftPaddle.y <= 580) {
       this.leftPaddle.y += this.paddleSpeed + this.spaceBoost;
     }
-    if (this.cursors.up.isDown && this.cursors.space.isDown) {
+    if (this.cursors.up.isDown && this.cursors.space.isDown && this.leftPaddle.y >= 20) {
       this.leftPaddle.y -= this.paddleSpeed + this.spaceBoost;
     }
-    if (this.cursors.down.isDown) {
+    if (this.cursors.down.isDown && this.leftPaddle.y <= 580) {
       this.leftPaddle.y += this.paddleSpeed;
     }
-    if (this.cursors.up.isDown) {
+    if (this.cursors.up.isDown && this.leftPaddle.y >= 20) {
       this.leftPaddle.y -= this.paddleSpeed;
     }
   }
