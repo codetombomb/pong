8a983bd077acec002717d86d2b397602b1ddda32
Tom Tobar <cocdetombomb@gmail.com>
2022-11-22 00:28:30 -0600

    add end game text

--- src/pong.js
+++ src/pong.js
@@ -7,14 +7,14 @@ export class Pong extends Phaser.Scene {
     this.score = 0;
     this.collidingWith = null;
     this.paddleSpeed = 8;
-    this.paddleSize = 1;
+    this.paddleSize = 0.45;
     this.ballSize = 0.5;
-    this.ballDirectionUp = true
+    this.ballDirectionUp = true;
     this.spaceBoost = 8;
     this.playerScores = {
       red: 0,
-      blue: 0
-    }
+      blue: 0,
+    };
   }
 
   preload() {
@@ -30,85 +30,97 @@ export class Pong extends Phaser.Scene {
   create() {
     this.cursors = this.input.keyboard.createCursorKeys();
     this.bounce = this.sound.add("bounce");
-    this.score = this.sound.add("score", {volume: 0.1});
-    
-    this.redScore = this.add.text(120, 120, `${this.playerScores.red}`, { fontSize: '80px', fill: 'red', fontFamily: "JustMyType" });
-    this.blueScore = this.add.text(680, 120, `${this.playerScores.blue}`, { fontSize: '80px', fill: 'blue', fontFamily: "JustMyType"});
-    
+    this.score = this.sound.add("score", { volume: 0.1 });
+
+    this.redScore = this.add.text(120, 120, `${this.playerScores.red}`, {
+      fontSize: "80px",
+      fill: "red",
+      fontFamily: "JustMyType",
+    });
+    this.blueScore = this.add.text(680, 120, `${this.playerScores.blue}`, {
+      fontSize: "80px",
+      fill: "blue",
+      fontFamily: "JustMyType",
+    });
+
     this.leftPaddle = this.physics.add.image(30, 300, "left-paddle");
     this.rightPaddle = this.physics.add.image(770, 300, "atlas", "rightPaddle");
     this.ball = this.physics.add.image(200, 300, "atlas", "ball");
-    
+
     this.leftPaddle.setScale(this.paddleSize);
     this.leftPaddle.body.setSize(10, 110, true);
     this.leftPaddle.body.setImmovable();
-    this.leftPaddle.body.setFrictionY(1)
-    
+    this.leftPaddle.body.setFrictionY(1);
+
     this.rightPaddle.setScale(this.paddleSize);
     this.rightPaddle.body.setSize(10, 110, true);
     this.rightPaddle.body.setImmovable();
-    this.rightPaddle.body.setFrictionY(1)
-    
+    this.rightPaddle.body.setFrictionY(1);
+
     this.ball.setDrag(0, 0);
     this.ball.body.bounce.set(1);
     this.ball.body.setSize(15, 15, true);
     this.ball.setScale(this.ballSize);
     this.ball.setCollideWorldBounds(true);
     this.ball.body.collideWorldBounds = true;
-    this.ball.body.setFrictionX(1)
-    this.ball.body.setFrictionY(1)
-    
+    this.ball.body.setFrictionX(1);
+    this.ball.body.setFrictionY(1);
+
     this.ball.body.onWorldBounds = true;
-    
+
     this.ball.setRandomPosition(277, 0, 267, 600);
     this.ball.setVelocityX(Math.floor(Math.random() * 101) - 400);
     this.ball.setVelocityY(Math.floor(Math.random() * 101) - 400);
   }
   update() {
     if (this.gameOver) return;
-    
+
     this.physics.collide(this.leftPaddle, this.ball);
     this.physics.add.collider(this.leftPaddle, this.ball, () => {
       if (this.cursors.down.isDown && this.ballDirectionUp) {
-        this.ball.body.setVelocityY((this.ball.body.velocity.y + 50) - (this.ball.body.velocity.y * 2))
-      } else if (this.cursors.up.isDown && !this.ballDirectionUp){
-        this.ball.body.setVelocityY((this.ball.body.velocity.y - 50) - (this.ball.body.velocity.y * 2))
+        this.ball.body.setVelocityY(
+          this.ball.body.velocity.y + 50 - this.ball.body.velocity.y * 2
+        );
+      } else if (this.cursors.up.isDown && !this.ballDirectionUp) {
+        this.ball.body.setVelocityY(
+          this.ball.body.velocity.y - 50 - this.ball.body.velocity.y * 2
+        );
       } else {
-        console.log("Paddle is stationary")
+        console.log("Paddle is stationary");
       }
       this.bounce.play();
     });
-    
+
     this.physics.collide(this.rightPaddle, this.ball);
     this.physics.add.collider(this.rightPaddle, this.ball, () => {
       this.bounce.play();
     });
-    
+
     this.physics.world.on("worldbounds", (body, up, down, left, right) => {
-      if(up && this.collidingWith !== "up") {
-        this.collidingWith = "up"
-        this.ballDirectionUp = false
-        this.bounce.play()
-      };
-      if(down && this.collidingWith !== "down") {
-        this.collidingWith = "down"
-        this.ballDirectionUp = true
-        this.bounce.play()
-      };
-      if(left && this.collidingWith !== "left") {
-        this.collidingWith = "left"
-        this.scorePoint("blue")
+      if (up && this.collidingWith !== "up") {
+        this.collidingWith = "up";
+        this.ballDirectionUp = false;
+        this.bounce.play();
       }
-      if(right && this.collidingWith !== "right") {
-        this.collidingWith = "right"
-        this.scorePoint("red")
+      if (down && this.collidingWith !== "down") {
+        this.collidingWith = "down";
+        this.ballDirectionUp = true;
+        this.bounce.play();
+      }
+      if (left && this.collidingWith !== "left") {
+        this.collidingWith = "left";
+        this.scorePoint("blue");
+      }
+      if (right && this.collidingWith !== "right") {
+        this.collidingWith = "right";
+        this.scorePoint("red");
       }
     });
 
     if (this.ball.y < this.rightPaddle.y) {
-      this.rightPaddle.y -= this.paddleSpeed - 2.75
+      this.rightPaddle.y -= this.paddleSpeed - 2.75;
     } else {
-      this.rightPaddle.y += this.paddleSpeed - 2.75
+      this.rightPaddle.y += this.paddleSpeed - 2.75;
     }
 
     if (
@@ -134,8 +146,32 @@ export class Pong extends Phaser.Scene {
   }
 
   scorePoint(side) {
-    this.playerScores[side] += 1 
-    this[`${side}Score`].setText(this.playerScores[side])
-    this.score.play()
+    this.playerScores[side] += 1;
+    this[`${side}Score`].setText(this.playerScores[side]);
+    if (this.playerScores[side] === 3) this.gameOver = true;
+    this.ball.destroy();
+    this.leftPaddle.destroy();
+    this.rightPaddle.destroy();
+    this.redScore.destroy();
+    this.blueScore.destroy();
+    this.score.play();
+    setTimeout(() => {
+      if (!this.gameOver) {
+        this.create();
+      } else {
+        const winner = this.playerScores.red === 3 ? "red" : "blue"
+        this.add.text(
+          255,
+          100,
+          `${winner.toUpperCase()} wins!`,
+          { fontSize: "100px", fill: winner, fontFamily: "JustMyType" }
+        );
+        this.add.text(180, 220, "Game Over", {
+          fontSize: "150px",
+          fill: "white",
+          fontFamily: "JustMyType",
+        });
+      }
+    }, 3000);
   }
 }
